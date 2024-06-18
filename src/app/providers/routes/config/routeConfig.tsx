71bfa21f88b4edc980205c2d5b2e1d7f6fe7fca1
Canyonz <Faton64rus@yandex.ru>
2024-06-18 18:23:31 +0300

    откат

--- src/app/providers/routes/config/routeConfig.tsx
+++ src/app/providers/routes/config/routeConfig.tsx
@@ -3,20 +3,15 @@ import { TodoPage } from "@/pages/todoPage";
 import { getRouteMain, getRouteTODO } from "@/shared/consts/routes";
 import { createBrowserRouter } from "react-router-dom";
 
-export const routeConfig = createBrowserRouter(
-	[
-		{
-			path: getRouteMain(),
-			element: <MainPage />,
-			errorElement: <div>Страница не найдена...</div>,
-		},
-		{
-			path: getRouteTODO(":id"),
-			element: <TodoPage />,
-			errorElement: <div>Страница не найдена...</div>,
-		},
-	],
+export const routeConfig = createBrowserRouter([
 	{
-		basename: "/AirNet/",
-	}
-);
+		path: getRouteMain(),
+		element: <MainPage />,
+		errorElement: <div>Страница не найдена...</div>,
+	},
+	{
+		path: getRouteTODO(":id"),
+		element: <TodoPage />,
+		errorElement: <div>Страница не найдена...</div>,
+	},
+]);
